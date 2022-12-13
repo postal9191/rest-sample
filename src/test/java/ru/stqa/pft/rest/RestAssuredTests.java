aae92165f194e03a89b2dd532d52487962475bdb
postal9191 <postal.9191@mail.ru>
2022-12-13 19:38:55 +0300

    Лекция 9.5

---
+++ src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -0,0 +1,47 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class RestAssuredTests {
+
+    @BeforeTest
+    public void init() {
+        RestAssured.authentication = RestAssured.basic("f2c01c8399d967884390d1e87b46b533", "");
+    }
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("I Add RestAssured").withDescription("into json RestAssured");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        Assert.assertEquals(newIssues, oldIssues);
+    }
+
+    private Set<Issue> getIssues() throws IOException {
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json); // парсим json
+        JsonElement issues = parsed.getAsJsonObject().get("issues"); // извлекаем по ключу нужную часть
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+    }
+
+    private int createIssue(Issue newIssue) throws IOException {
+        String json = RestAssured.given()
+                .parameter("subject", newIssue.getSubject())
+                .parameter("description", newIssue.getDescription())
+                .post("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+}
